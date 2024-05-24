61d2408f7b6a6dd80169ab6b952667eed5f33ddf
rohitgr4 <rohit4grover@gmail.com>
2024-05-24 17:52:24 +0530

    UPS Tasks
    UPS Tasks

---
+++ Task2/UserListAPI/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using UserListAPI.Domain;
+
+namespace UserListAPI
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : Controller
+    {        
+        private static readonly List<Users> Users = new List<Users>
+        {
+            new Users { UserId = 1, UserName = "Albert Miguel", UserEmail = "albert.miguel@example.com" },
+            new Users { UserId = 2, UserName = "Jona", UserEmail = "jane.smith@example.com" },
+            new Users { UserId = 3, UserName = "Michael Johnson", UserEmail = "michael.johnson@example.com" },
+            new Users { UserId = 4, UserName = "Emily Ross", UserEmail = "emily.ross@example.com" },            
+        };
+
+        [HttpGet]        
+        public ActionResult<IEnumerable<Users>> GetUsers()
+        {
+            return Users;
+        }
+    }
+
+}
